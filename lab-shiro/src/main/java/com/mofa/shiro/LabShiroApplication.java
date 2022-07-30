e4f1557bde10f254cced3c54a8b67f2c56100283
amaze <mjwu9551@163.com>
2022-07-30 23:39:18 +0800

    1.shiro demo

---
+++ lab-shiro/src/main/java/com/mofa/shiro/LabShiroApplication.java
@@ -0,0 +1,13 @@
+package com.mofa.shiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabShiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabShiroApplication.class, args);
+    }
+
+}
---
+++ lab-shiro/src/test/java/com/mofa/shiro/LabShiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mofa.shiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabShiroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
